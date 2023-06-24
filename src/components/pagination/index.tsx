d3b662bfdca1e8da36afa3899c400ca87fde7d74
Deivz <carlosd_oliveira@hotmail.com>
2023-06-24 15:45:41 -0300

    Filtro implementado, reajustar css do botão do filtro. Logo modificada. Inicio da implementação da página 404, implementar estilização. Estilizar navbar, implementar paginação dos cards, melhorar estilo no geral.

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,21 +1,21 @@
-import styles from './pagination.module.css'
+// import styles from './pagination.module.css'
 
-type PaginationProps = {
-   limit: number;
-   offset: number;
-   total: number;
-}
+// type PaginationProps = {
+//    limit: number;
+//    offset: number;
+//    total: number;
+// }
 
-const PAGE = 1;
-const MAX_ITEMS = 9;
-const MAX_LEFT = (MAX_ITEMS - PAGE) / 2;
+// const PAGE = 1;
+// const MAX_ITEMS = 9;
+// const MAX_LEFT = (MAX_ITEMS - PAGE) / 2;
 
-export default function Pagination({ limit, offset, total }: PaginationProps) {
+// export default function Pagination({ limit, offset, total }: PaginationProps) {
 
-   const currentPage = offset ? offset/limit + PAGE : PAGE;
-   const totalPages = Math.ceil(offset/limit);
+//    const currentPage = offset ? offset/limit + PAGE : PAGE;
+//    const totalPages = Math.ceil(offset/limit);
 
-   return (
-      <div>Paginação</div>
-   )
-}
+//    return (
+//       <div>Paginação</div>
+//    )
+// }
