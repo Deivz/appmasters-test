1bca6130c1bcfa99f02bbd6c9f6c8d6e95aed136
Deivz <carlosd_oliveira@hotmail.com>
2023-07-13 22:46:43 -0300

    Filtros e busca implementados com sucesso. Listar favoritos e exibir dados vindo do firebase. Exibir erros caso o usuário não consiga favoritar ou avaliar. Verificar por que vercel não lê o .env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyB3I92NPp4mMuL1BCSlR4fs5bXEHlPt6T8",
+  authDomain: "appmasters-test.firebaseapp.com",
+  projectId: "appmasters-test",
+  storageBucket: "appmasters-test.appspot.com",
+  messagingSenderId: "537539760401",
+  appId: "1:537539760401:web:aaa7de4c72b94a9d067cb0",
+  measurementId: "G-54FDT0KX6B",
 };
 
 const app = initializeApp(firebaseConfig);
