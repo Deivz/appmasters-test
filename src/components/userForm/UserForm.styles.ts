1da6fb21e1a9d65aa95dfa7ecb4d159507fab801
Deivz <carlosd_oliveira@hotmail.com>
2023-07-15 00:59:08 -0300

    Listar favoritos. Exibir erros caso o usuário não consiga favoritar ou avaliar. Verificação básica de formulário implementada, avaliar refatoração. Verificar por que vercel não lê o .env. Codigo refatorado, multifiltros implementado. Verificar motivo de não fazer reload após login e logout

--- src/components/userForm/UserForm.styles.ts
+++ src/components/userForm/UserForm.styles.ts
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 
 export const FormContainer = styled.form`
+  align-items: center;
   display: flex;
   flex-direction: column;
   justify-content: center;
